7b17f3535ebbe54c2e573122f313225223c8fa5e
mkzopio <mk@zopio.com>
2025-06-30 06:37:37 +0300

    fix: testing package linting errors

---
+++ packages/testing/src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * This is a placeholder file to satisfy the TypeScript compiler.
+ * The @repo/testing package is primarily a configuration package for Vitest
+ * and doesn't contain actual TypeScript code.
+ */
+
+export {};
